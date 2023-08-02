ba199186a684dba6da42f0b025deb5ce5aeb7342
IvSan <foritun@gmail.com>
2023-08-02 20:15:39 +0200

    Test to check if opponent's big pit skipped while sowing

--- src/test/java/dev/ivsan/bolassessment/service/KalahaGameEngineImplTest.java
+++ src/test/java/dev/ivsan/bolassessment/service/KalahaGameEngineImplTest.java
@@ -2,12 +2,17 @@ package dev.ivsan.bolassessment.service;
 
 import dev.ivsan.bolassessment.model.Board;
 import dev.ivsan.bolassessment.model.GameState;
+import dev.ivsan.bolassessment.model.Pit;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static dev.ivsan.bolassessment.utils.BoardUtils.BOARD_PITS_LENGTH;
 import static dev.ivsan.bolassessment.utils.BoardUtils.encodeBoardState;
-import static dev.ivsan.bolassessment.utils.BoardUtils.getVisualRepresentation;
 import static dev.ivsan.bolassessment.utils.TestDataGenerator.aBoard;
+import static dev.ivsan.bolassessment.utils.TestDataGenerator.aPlayer;
 import static dev.ivsan.bolassessment.utils.TestDataGenerator.singleMoveVictoryBoard;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -28,9 +33,7 @@ class KalahaGameEngineImplTest {
         assertEquals("0-7.0.7.7.7.0.2.-8.1.8.8.8.8.1.", encodeBoardState(board));
         kalahaGameEngine.processMove(board, 1);
         assertEquals("1-7.0.7.7.7.0.2.-8.0.9.8.8.8.1.", encodeBoardState(board));
-        System.out.println(getVisualRepresentation(board));
         kalahaGameEngine.processMove(board, 4);
-        System.out.println(getVisualRepresentation(board));
         assertEquals("0-7.0.7.7.0.1.3.-9.1.10.9.9.8.1.", encodeBoardState(board));
         assertEquals(GameState.IN_PROGRESS, board.getState());
     }
@@ -52,6 +55,31 @@ class KalahaGameEngineImplTest {
         kalahaGameEngine.processMove(board, 5);
         assertEquals("0-0.0.0.0.0.0.1.-6.6.6.6.6.6.0.", encodeBoardState(board));
         assertEquals(GameState.SOUTH_WIN, board.getState());
+    }
+
+    @Test
+    void testSkipOpponentsBigPitWhileSowing() {
+        Board board = Board.builder()
+                .northPlayer(aPlayer())
+                .northPits(
+                        Stream.concat(
+                                Stream.generate(Pit::new).limit(BOARD_PITS_LENGTH - 1),
+                                Stream.generate(() -> new Pit(0, Pit.PitType.BIG)).limit(1)
+                        ).collect(Collectors.toList())
+                )
+                .southPlayer(aPlayer())
+                .southPits(List.of(
+                        new Pit(0, Pit.PitType.REGULAR),
+                        new Pit(0, Pit.PitType.REGULAR),
+                        new Pit(0, Pit.PitType.REGULAR),
+                        new Pit(0, Pit.PitType.REGULAR),
+                        new Pit(0, Pit.PitType.REGULAR),
+                        new Pit(10, Pit.PitType.REGULAR),
+                        new Pit(0, Pit.PitType.BIG)
+                ))
+                .build();
+        kalahaGameEngine.processMove(board, 5);
+        assertEquals("0-1.1.1.0.0.0.1.-7.7.7.7.7.7.0.", encodeBoardState(board));
     }
 
 }
